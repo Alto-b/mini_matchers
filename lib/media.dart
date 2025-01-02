6a2426024c2bd43422901ca1e5503885906e0a86
Alto b <altob282@gmail.com>
2025-01-02 15:50:51 +0530

    assets url updated

--- lib/media.dart
+++ lib/media.dart
@@ -1,4 +1,9 @@
 class Media {
+  //app logo
   static const String appIcon = 'assets/logo_with_bg.png';
   static const String appIconNoBg = 'assets/logo_with_no_bg.png';
+
+  //lottie
+  static const String homePagePurpleDots = 'assets/lottie/dots_bg.json';
+  static const String playButton = 'assets/lottie/play_green_circle.json';
 }
